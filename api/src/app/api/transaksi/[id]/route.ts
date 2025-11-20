2666c4617b9a951a6cd6eb0bbf8414365e11d1ba
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 16:26:41 +0700

    membuat variable body

--- api/src/app/api/transaksi/[id]/route.ts
+++ api/src/app/api/transaksi/[id]/route.ts
@@ -42,4 +42,8 @@ export async function GET(
 export async function PUT(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
-) {}
+) {
+    const { id } = await params;
+    const body = await request.json();
+    const { userId, produkId, totalHarga } = body;
+}
