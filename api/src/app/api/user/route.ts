4e6340bcd177c0761b3c859e970b59c9de800bee
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 16:55:42 +0700

    fix error

--- api/src/app/api/user/route.ts
+++ api/src/app/api/user/route.ts
@@ -38,7 +38,7 @@ export async function POST(request: NextRequest) {
         name,
         username,
         password, // Note: Hash password in production!
-      }
+      },
       select: {
         id: true,
         name: true,
